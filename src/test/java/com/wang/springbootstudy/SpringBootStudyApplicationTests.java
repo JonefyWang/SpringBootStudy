81e556be131ead2f1db4fdfd97b5ecd3a9b960a0
JonefyWang <wangyuanmimi@163.com>
2022-08-28 18:35:56 +0800

    Initial commit

---
+++ src/test/java/com/wang/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
